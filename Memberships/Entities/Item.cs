753d69fce554455fb58d66f929cc39b789d4cec9
loadtor <loadtor@ASUS-LOADTOR>
2016-09-18 21:08:44 +0700

    Add project files.

---
+++ Memberships/Entities/Item.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Memberships.Entities
+{
+    [Table("Item")]
+    public class Item
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [MaxLength(255)]
+        [Required]
+        public string Title { get; set; }
+        [MaxLength(1024)]
+        public string Description { get; set; }
+        [MaxLength(1024)]
+        public string Url { get; set; }
+        [MaxLength(1024)]
+        [DisplayName("Image Url")]
+        public string ImageUrl { get; set; }
+        [AllowHtml]
+        public string HTML { get; set; }
+        [DefaultValue(0)]
+        [DisplayName("Wait Days")]
+        public int WaitDays { get; set; }
+        [NotMapped]
+        public string HTMLShort { get { return HTML == null || HTML.Length < 50 ? HTML : HTML.Substring(0, 50); } }
+        public int ItemTypeId { get; set; }
+        public int SectionId { get; set; }
+        public int PartId { get; set; }
+        [DisplayName("Is Free")]
+        public bool IsFree { get; set; }
+        public virtual ICollection<Section> Sections { get; set; }
+        public virtual ICollection<Part> Parts { get; set; }
+        [DisplayName("Item Types")]
+        public virtual ICollection<ItemType> ItemTypes { get; set; }
+
+
+
+    }
+}
