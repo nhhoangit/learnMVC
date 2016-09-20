3190653105ab04463c445052b393e97ff262eab0
administrator <administrator@VWR-NTRAN28>
2016-09-19 23:39:01 -0400

    Completed to define structure

--- Memberships/Models/IdentityModels.cs
+++ Memberships/Models/IdentityModels.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Memberships.Entities;
+using System;
 
 namespace Memberships.Models
 {
@@ -17,6 +18,11 @@ namespace Memberships.Models
             // Add custom user claims here
             return userIdentity;
         }
+
+        public string FirstName { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime Registered{ get; set; }
+
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
@@ -31,14 +37,16 @@ namespace Memberships.Models
             return new ApplicationDbContext();
         }
 
-        public DbSet<Section> Section { get; set; }
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<Part> Parts { get; set; }
+        public DbSet<ItemType> ItemTypes { get; set; }
         public DbSet<Item> Items { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductType> ProductTypes { get; set; }
         public DbSet<ProductLinkText> ProductLinkTexts { get; set; }
-        public DbSet<Subscription> Subscription { get; set; }
+        public DbSet<Subscription> Subscriptions { get; set; }
         public DbSet<ProductItem> ProductItems { get; set; }
-        public DbSet<SubscriptionProduct> SubscriptionProduct { get; set; }
+        public DbSet<SubscriptionProduct> SubscriptionProducts { get; set; }
         public DbSet<UserSubscription> UserSubscriptions { get; set; }
     }
 }
